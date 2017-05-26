34aaf5bd5ef933ebe23f018330a931889eb40995
Angel Ortega <angelortega@umail.ucsb.edu>
2017-05-26 16:35:18 -0700

    update some minor changes

--- hw4-2/kerpercep.py
+++ hw4-2/kerpercep.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import sys, re
 import numpy as np
+import math
 
 '''
     Author: Angel Gabriel Ortega 🙃
@@ -17,6 +18,8 @@ def interpret_output_filename(file_with_digits):
         outputfile = 'output.txt'
     return outputfile
 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
 def load_data(training_file):
     '''
@@ -33,8 +36,34 @@ def load_data(training_file):
     data = np.loadtxt(training_file, skiprows=1, usecols=range(0, point_dimensionality))
     return data, num_points, point_dimensionality
 
+class PerceptronModel():
 
-# def kernelalize_matrix()
+    def __init__(self, predictors, targets, sigma):
+        self.predictors = predictors
+        self.targets = targets
+        self.size_of_train = len(predictors)
+        self.sigma = sigma
+        false_positives = None
+        false_negatives = None
+
+    def converge_train_weights(self, num_iterations, sigma):
+        delta_alph = np.zeros(self.size_of_train)
+        for j in range(num_iterations):
+            for i in range(self.size_of_train):
+                K_mat_similarities = self.RBF_gaussian_kernal(self.predictors, self.targets, sigma)
+                hypothesis = np.dot(np.linalg.multiply(delta_alph, self.targets), K_mat_similarities.T)
+                if sigmoid(hypothesis) > 0.5:
+                    predicted = 1
+                else:
+                    predicted = 0
+                if predicted != self.predictors(i):
+                    delta_alph = delta_alph + 1
+
+        return delta_alph
+
+    def RBF_gaussian_kernal(self, x, y, sigma):
+        similarities = np.exp(- np.linalg.norm(x - y) / (2 * sigma ** 2))
+        return similarities
 
 
 if __name__ == "__main__":
@@ -61,15 +90,29 @@ if __name__ == "__main__":
 
     # Get targets, also known as Y, actual, or labels
     pos_training_targets = np.ones(pos_num_training_samples, dtype=int)
-    neg_training_targets = np.zeros(neg_num_training_samples, dtype=int)
-    y_training_target = np.concatenate((pos_training_targets, neg_training_targets), axis=0)
+    neg_training_targets = np.full(shape=(neg_num_training_samples,), fill_value= -1, dtype=int)
+    y_training_targets = np.concatenate((pos_training_targets, neg_training_targets), axis=0)
 
     pos_testing_targets = np.ones(pos_num_testing_samples, dtype=int)
-    neg_testing_targets = np.zeros(neg_num_testing_samples, dtype=int)
-    y_testing_target = np.concatenate((pos_testing_targets, neg_testing_targets), axis=0)
+    neg_testing_targets = np.full(shape=(neg_num_testing_samples,1), fill_value=-1, dtype=int)
+    y_testing_targets = np.concatenate((pos_testing_targets, neg_testing_targets), axis=0)
+
+    # support_vectors =
+    #  = converge_for_min(training_predictors, y_training_target)
+    # kernel_mat = radial_basis_function(training_predictors, )
+
+    clf_model = PerceptronModel(training_predictors, y_training_targets, sigma)
+    alphas = clf_model.converge_train_weights(num_iterations=len(training_predictors)+len(testing_predictors))
+
+    pos_hits =  np.logical_not(np.logical_xor(clf_model.RBF_gaussian_kernal(testing_predictors, y_testing_targets, sigma), y_testing_targets))
+
+    error_rate = 1 - (pos_hits / float(len(y_testing_targets)))
 
 
-    # kernel_mat =
+    with open(output_filename, 'w') as file:
+        line = ' '.join('{0}'.format(v, i) for i, v in enumerate(alphas))
+        file.write('Alphas: ' + line + '\n')
+        print 'Error'
 
     #
     # testing_data, test_num_points, testing_dimensionality = load_data(testing_file)
