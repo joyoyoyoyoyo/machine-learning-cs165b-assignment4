b1d0ae11f2e77a2a1ea9a27644b85caa100a3fc1
Angel Ortega <an22ortega@gmail.com>
2017-05-26 02:12:05 -0700

    adjusting for k votes :heavy_check_mark:

--- hw4-3/knn.py
+++ hw4-3/knn.py
@@ -32,7 +32,7 @@ def euclidian(x, y):
     diff = delta * delta
     return sum(diff)
 
-def knn(training_data, testing_data, labels, outputfilename):
+def knn(k, training_data, testing_data, labels, outputfilename):
     print 'knn starting...'
 
     output = {}
@@ -40,20 +40,40 @@ def knn(training_data, testing_data, labels, outputfilename):
         for i, test_sample in enumerate(testing_data):
             distances = {}
             min_dist = None
+            global_min = None
+            max_overall = {}
+            distances_mutable = {}
+            votes = {key: 0 for key in labels}
             for train_sample, label in zip(training_data, labels):
                 distance = euclidian(train_sample, test_sample)
                 if distance not in distances:
                     distances[distance] = label
+                    distances_mutable[distance] = label
                 else:
                     distance[distance] = min(distances[distance], label)
+                    distances_mutable[distance] = min(distances[distance], label)
 
-            min_dist = min(distances.keys())
-            predicted = distances[min_dist]
-            output[min_dist] = predicted
-            # line = ' '.join('{0:.4f}'.format(v, i) for i, v in enumerate(test_sample))
-            # label = predicted
+            # TODO: if k < num points
+            # TODO: on ties, pick lowest class label, check for ties
+            for num in range(k):
+                min_dist = min(distances_mutable.keys())
+                predicted = distances[min_dist]
+                votes[predicted]+= 1
+                del distances_mutable[min_dist]
+            # output[min_dist] = predicted
+
+            # max_overall = max(votes, key= lambda key: votes[key])
+            max_overall = [v for v in sorted(votes.items(), key=lambda kv: (-kv[1], kv[0]))]
+
+            min_dict = max_overall[0]
+            predicted = min_dict[0]
+            # predicted = [v[0] for v in sorted(votes.items(), key=lambda kv: (-kv[1], kv[0]))
+
+            line = ' '.join('{0:.4f}'.format(v, i) for i, v in enumerate(test_sample))
+            label = str(predicted)
             # file.write(str(i + 1) + '. ' + str(line) + ' -- ' + str(int(label)) + '\n')
-            file.write('{0} . Minimum:\t{1:.4f} -- {2}\n'.format(i+1, min_dist, int(label))
+            file.write('{0} . {1} -- {2}\n'.format(i+1, line, predicted))
+            # file.write('{0} . Minimum:\t{1:.4f} -- {2}\n'.format(i+1, min_dist, int(predicted)))
 
     file.close()
 
@@ -80,7 +100,7 @@ if __name__ == "__main__":
 
     testing_data, test_num_points, testing_dimensionality = load_data(testing_file)
 
-    predicted = knn(training_data, testing_data, labels, outputfilename='output1.txt')
+    predicted = knn(5, training_data, testing_data, labels, outputfilename='output2.txt')
 
     # write_to_output(predicted, 'output1.txt')
 
