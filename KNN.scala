7a8e7fb7c1e766714e35daf4f848188fb6c8f72f
Angel Ortega <an22ortega@gmail.com>
2017-07-07 06:32:31 -0700

    SparkSQL getting started and Spark is RUNning

--- KNN.scala
+++ KNN.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 //import org.apache.spark.SparkContext
 //import org.apache.spark.SparkContext._
 
@@ -6,7 +7,13 @@ object KNN {
 
   def main(args: Array[String]) {
     val train = "hw4-3/training_data_prob3_1.txt"
-    val conf = new SparkConf().setAppName("KNN (K-nearest-neighbour)")
-    
+    //val conf = new SparkConf().setAppName("KNN (K-nearest-neighbour)")
+    sqlTesting(train)
+  }
+
+  def sqlTesting(dataFile: String) {
+    val spark = SparkSession.builder
+      .appName("KNN (K-nearest-neighbour)")
+      .getOrCreate()
   }
 }
--- build.sbt
+++ build.sbt
@@ -13,7 +13,8 @@ val sparkVersion = "2.1.1"
 //)
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % sparkVersion
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
   // sparkVersion % "provided"
 )
 
