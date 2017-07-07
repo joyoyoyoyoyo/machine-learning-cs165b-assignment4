daf7b7b2d75ef11d8b532927bfb58cb72ad690d3
Angel Ortega <an22ortega@gmail.com>
2017-07-07 06:02:20 -0700

    SparkConf runs with new build.sbt

--- KNN.scala
+++ KNN.scala
@@ -1,8 +1,12 @@
-
+import org.apache.spark.SparkConf
+//import org.apache.spark.SparkContext
+//import org.apache.spark.SparkContext._
 
 object KNN {
 
   def main(args: Array[String]) {
-
+    val train = "hw4-3/training_data_prob3_1.txt"
+    val conf = new SparkConf().setAppName("KNN (K-nearest-neighbour)")
+    
   }
 }
